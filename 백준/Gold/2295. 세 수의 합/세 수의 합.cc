9ebb4c7ad004322753873951b5a7d73a54af7b5a
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-07-08 18:18:46 +0900

    [Gold IV] Title: 세 수의 합, Time: 404 ms, Memory: 44628 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -2,14 +2,12 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <unordered_set>
 #include <unordered_map>
 
 using namespace std;
 
 int N, res = 0;
 vector<int> v;
-unordered_set<int> us;
 unordered_map<int, int> um;
 
 int main() {
@@ -19,15 +17,14 @@ int main() {
     v.resize(N);
     for (int i = 0; i < N; ++i) cin >> v[i];
     sort(v.begin(), v.end());
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = i; j < N; ++j) {
-            us.insert(v[j] + v[i]);
-            um.insert({ v[j] - v[i], v[j] });
+    
+    for (int y = N - 1; y >= 0; --y) {
+        for (int z = y; z < N; ++z) {
+            um[v[z] - v[y]] = v[z];
         }
-    }
-    for (int target : us) {
-        res = max(res, um[target]);    
+        for (int x = 0; x <= y; ++x) {
+            res = max(res, um[v[x] + v[y]]);
+        }
     }
     cout << res;
     return 0;
