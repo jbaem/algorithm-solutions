e421ae777ba2b343c57873c3f75f4ed98a69f08c
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-07-13 17:32:21 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,32 +1,25 @@
 #define fastio() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <stack>
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
-int N;
-vector<int> v, res;
-stack<pii> st;
+int N, tmp;
+stack<pair<int, int>> st;
 
 int main() {
     fastio();
 
     cin >> N;
-    v.resize(N + 1);
-    res.resize(N + 1);
-    for (int i = 1; i <= N; ++i) cin >> v[i];
+    for (int i = 1; i <= N; ++i) {
+        cin >> tmp;
 
-    for (int i = N; i > 0; --i) {
-        while (!st.empty() && st.top().first <= v[i]) {
-            res[st.top().second] = i;
+        while (!st.empty() && st.top().first < tmp) {
             st.pop();
         }
-        st.push({ v[i], i });
+        cout << (st.empty() ? 0 : st.top().second) << " ";
+        st.push({ tmp, i });
     }
-    for (int i = 1; i <= N; ++i) cout << res[i] << " ";
     return 0;
 }
