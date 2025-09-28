3ce44c849a81953663ef2ead228fe5c11cafa67f
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2025-09-28 22:53:29 +0900

    [Gold I] Title: 소수의 곱, Time: 1116 ms, Memory: 6776 KB -BaekjoonHub

---
+++ 백준/Gold/2014. 소수의 곱/소수의 곱.cc
@@ -0,0 +1,39 @@
+#define fastio() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <climits>
+
+
+using namespace std;
+
+int K, N;
+vector<int> v;
+set<int> s;
+
+int main() {
+    fastio();
+
+    cin >> K >> N;
+    for (int i = 0; i < K; ++i) {
+        int tmp; cin >> tmp;
+        v.push_back(tmp);
+        s.insert(tmp);
+    }
+
+    int curr;
+    for (int i = 0; i < N; ++i) {
+        curr = *s.begin();
+        s.erase(curr);
+
+        for (int n : v) {
+            long long next = (long long)curr * n;
+            if (next >= INT_MAX) continue;
+            s.insert(next);
+            if (s.size() > N) s.erase(*s.rbegin());
+        }
+    }
+    cout << curr;
+    return 0;
+}
